04e53080463365114a1c98587160cd48746b894f
Josue Abraham <josuea@noukod.com>
2019-02-20 13:41:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(employees){
+    for(let i =0; i < employees.length; i++){
+        console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+    }
+    return employees;
+}
+
+function tailsNeverFails(){
+    // tails = ;
+    let nbreTails = 0;
+    while(Math.random() >= 0.5){
+        nbreTails++;
+    }
+    return `You got ${nbreTails} tails in a row!`;
+}
